bc2d4864471fd5661557c252839180f3c218188d
Nathan Chancellor <nathan@kernel.org>
2022-06-23 14:44:08 -0700

    lx2160a_uefi: Add personal build script
    * Set SoC speed to 2.2GHz
    * Set bus speed to 750MHz
    * Set memory speed to 3200MHz
    * Enable XMP
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ build.fish
@@ -0,0 +1,24 @@
+#!/usr/bin/env fish
+
+in_container_msg -h; or return
+
+set root (status dirname)
+set tag (basename $root)
+
+rm -fr $root/images
+
+podman build \
+    -t $tag \
+    $root/docker/; or return
+
+podman run \
+    --env BUS_SPEED=750 \
+    --env CLEAN=1 \
+    --env DDR_SPEED=3200 \
+    --env SOC_SPEED=2200 \
+    --env XMP_PROFILE=1 \
+    --interactive \
+    --rm \
+    --tty \
+    --volume $root:/work:Z \
+    $tag build
